a833e1ddc4c20f931df1cc37e2b468cb9e231824
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2019-05-10 16:20:05 -0700

    mjc - added tests in tests.sh

--- lab2b/tests.sh
+++ lab2b/tests.sh
@@ -7,22 +7,69 @@ if [ ! -f "$FILE_LIST" ]; then
     exit 1
 fi
 
-# results for mutex and spin lock, used for lab2b_1.png
-threads=(1 2 4 8 12 16 24)
-iteration=1000
-syncs=("s" "m")
-for thread in "${threads[@]}"
-do
-    for sync in "${syncs[@]}"
-    do
-        ./"$FILE_LIST" --iterations="$iteration" --threads="$thread" --sync="$sync" >> "$LOG_FILE_LIST"
-    done
-done
+### Part I: results for mutex and spin lock, used for lab2b_1.png
+#threads=(1 2 4 8 12 16 24)
+#iteration=1000
+#syncs=("s" "m")
+#for thread in "${threads[@]}"
+#do
+#    for sync in "${syncs[@]}"
+#    do
+#        ./"$FILE_LIST" --iterations="$iteration" --threads="$thread" --sync="$sync" >> "$LOG_FILE_LIST"
+#    done
+#done
+#
+### Part III: timing mutex, used for lab2b_2.png
+#threads=(1 2 4 8 12 16 24)
+#iteration=1000
+#for thread in "${threads[@]}"
+#do
+#    ./"$FILE_LIST" --iterations="$iteration" --threads="$thread" --sync=m >> "$LOG_FILE_LIST"
+#done
+#
+### Part IV: using sublists
+#
+## yield=id w/o protection
+#list=4
+#threads=(1 4 8 12 16)
+#iterations=(1 2 4 8 16)
+#for thread in "${threads[@]}"
+#do
+#    for i in "${iterations[@]}"
+#    do
+#        ./"$FILE_LIST" --iterations="$i" --threads="$thread" --yield=id --lists="$list" >> "$LOG_FILE_LIST"
+#    done
+#done
+#
+## yield=id w/ protection
+#list=4
+#threads=(1 4 8 12 16)
+#iterations=(10 20 40 80)
+#syncs=("s" "m")
+#for thread in "${threads[@]}"
+#do
+#    for i in "${iterations[@]}"
+#    do
+#        for sync in "${syncs[@]}"
+#        do
+#            ./"$FILE_LIST" --iterations="$i" --threads="$thread" --yield=id --sync="$sync" --list="$list" >> "$LOG_FILE_LIST"
+#        done
+#    done
+#done
 
-# timing mutex, used for lab2b_2.png
-threads=(1 2 4 8 12 16 24)
+# rerun synchronized versions without yield
 iteration=1000
+threads=(1 2 4 8 12)
+lists=(1 4 8 16)
+syncs=("s" "m")
+
 for thread in "${threads[@]}"
 do
-    ./"$FILE_LIST" --iterations="$iteration" --threads="$thread" --sync=m >> "$LOG_FILE_LIST"
+    for list in "${lists[@]}"
+    do
+        for sync in "${syncs[@]}"
+        do
+            ./"$FILE_LIST" --iterations="$iteration" --threads="$thread" --sync="$sync" --list="$list" >> "$LOG_FILE_LIST"
+        done
+    done
 done
